40202ccef2a56c51f902f1b73199fc3284c0f383
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-09-17 07:58:28 -0400

    Removed commented out code. Added IDs to card buttons. Setup jQuery links for buttons. Changed card images for two cards

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
-    
+
+// First card buttons
+ $("#button1-git").click(function() {
+    window.open("https://github.com/drosenbaum5/AdventureGame", '_blank');
+ })
+ $("#button1-site").click(function() {
+    window.open("https://solarsystemadventure.herokuapp.com/", '_blank');
+ })
+
+// Second card buttons 
+ $("#button2-git").click(function() {
+    window.open("https://github.com/drosenbaum5/HarryPotter", '_blank');
+ })
+
+ $("#button2-site").click(function() {
+    window.open("https://harrypotter-csd.herokuapp.com", '_blank');
+ })
+ 
+//Third card buttons
+ $("#button3-git").click(function() {
+    window.open("https://github.com/drosenbaum5/destination_podcast", '_blank');
+ })
+
+ $("#button3-site").click(function() {
+    window.open("https://drosenbaum5.github.io/destination_podcast/", '_blank');
+ })
+ 
+// Fourth card buttons
+$("#button4-git").click(function() {
+    window.open("https://github.com/drosenbaum5/FriendFinder", '_blank');
+ })
+
+ $("#button4-site").click(function() {
+    window.open("https://polar-peak-64793.herokuapp.com/", '_blank');
+ })
 })
